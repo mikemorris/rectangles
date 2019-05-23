ea225d100788697efa1fbeda395fbfdfc49c6d21
Mike Morris <mikemorris@users.noreply.github.com>
2019-05-23 10:05:06 -0400

    add can_hold method

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ impl Rectangle {
     fn rename(&mut self, name: &str) {
         self.name = name.to_string();
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -27,4 +31,23 @@ fn main() {
     rect.rename("bar");
 
     println!("{:#?}", rect);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+        name: String::from("a"),
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+        name: String::from("b"),
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+        name: String::from("c"),
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
