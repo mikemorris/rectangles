4fa771fe287834527982949574e7226c94336d15
Mike Morris <mikemorris@users.noreply.github.com>
2019-05-23 22:57:50 -0400

    move shape module into new file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,4 @@
-mod shape {
-    #[derive(Debug, Default)]
-    pub struct Rectangle {
-        width: u32,
-        height: u32,
-        name: Option<String>,
-    }
-
-    impl Rectangle {
-        pub fn new(width: u32, height: u32) -> Rectangle {
-            Rectangle {
-                width,
-                height,
-                ..Default::default()
-            }
-        }
-
-        pub fn area(&self) -> u32 {
-            self.width * self.height
-        }
-
-        pub fn set_name(&mut self, name: &str) {
-            self.name = Some(name.to_string());
-        }
-
-        pub fn can_hold(&self, other: &Rectangle) -> bool {
-            self.width > other.width && self.height > other.height
-        }
-    }
-}
+mod shape;
 
 fn main() {
     use shape::Rectangle;
